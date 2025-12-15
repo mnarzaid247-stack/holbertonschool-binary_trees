a4fda8a64f4314800b680a4685c6d9c8c41152fc
Aljawharah <aljawharhs18@gmail.com>
2025-12-15 18:01:13 +0300

    cleaner code

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -4,21 +4,17 @@
 * @tree: pointer to the root node of the tree
 * Return: balance factor of the tree, or 0 if tree is NULL
 *
-* Balance Factor Concept
+* Description:
 * The balance factor (BF) of a node in a binary tree is:
 * Height of left subtree − Height of right subtree
+*
 * Left heavy → positive BF
 * Right heavy → negative BF
 * Perfectly balanced → BF = 0
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int BF, left_height, right_height;
-
 	if (!tree)
 		return (0);
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-	BF = left_height - right_height;
-	return (BF);
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
