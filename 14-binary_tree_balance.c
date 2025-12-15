b2864f56ed4e343019bd743cadbf0d90a7e530ce
Aljawharah <aljawharhs18@gmail.com>
2025-12-15 18:30:29 +0300

    modified the height function in balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,18 +8,15 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-size_t i;
-size_t j;
+size_t i = 0, j = 0;
 if (tree == NULL)
 	return (0);
-if (tree->left == NULL && tree->right == NULL)
-return (0);
-i = binary_tree_height(tree->right);
-j = binary_tree_height(tree->left);
-if (i >= j)
+
+i += binary_tree_height(tree->right);
+j += binary_tree_height(tree->left);
+if (i > j)
 	return (i + 1);
-else
-	return (j + 1);
+return (j + 1);
 }
 
 /**
