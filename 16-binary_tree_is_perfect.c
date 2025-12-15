a698ede210dd9b1752fbfa5f45db569af16a4651
Aljawharah <aljawharhs18@gmail.com>
2025-12-15 19:00:08 +0300

    tweak

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -40,7 +40,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if (!binary_tree_is_full(tree))
+	if (binary_tree_is_full(tree) == 0)
 		return (0);
 	if (binary_tree_nodes(tree->left) == binary_tree_nodes(tree->right))
 		return (1);
